bd8a408a149128bb840b2ddd679ba529c20aff0c
Matthew Grasmick <matt.grasmick@gmail.com>
2017-12-21 11:40:44 -0500

    Abstracting expander logic to grasmash/expander.

---
+++ src/Stringifier.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace Grasmash\YamlExpander;
+
+use Grasmash\Expander\StringifierInterface;
+use Symfony\Component\Yaml\Yaml;
+
+class Stringifier implements StringifierInterface
+{
+    /**
+     * Converts array to string.
+     *
+     * @param array $array
+     *   The array to convert.
+     *
+     * @return string
+     *   The resultant string.
+     */
+    public static function stringifyArray(array $array)
+    {
+        return Yaml::dump($array, 0);
+    }
+}
